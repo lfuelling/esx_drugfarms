63053a86d0f4f9f2442ecc56afa46ef4ff5100f6
Fly Man <fly.man.opensim@gmail.com>
2019-10-03 00:32:23 +0200

    Dutch locales

---
+++ locales/nl.lua
@@ -0,0 +1,13 @@
+Locales['nl'] = {
+    ['no_access'] = "~r~Je kunt er niet in.",
+    ['weed_farm'] = "Weed Farm",
+    ['counterfeit'] = "Counterfeit",
+    ['document_forge'] = "Document Forge",
+    ['meth_lab'] = "Meth Lab",
+    ['coke_lab'] = "Coke Lab",
+    ['bunker'] = "Bunker",
+    ['doomsday_facility'] = "Doomsday Facility",
+    ['biker_clubhouse'] = "Clubhouse",
+    ['press_to_enter'] = 'Druk op ~INPUT_CONTEXT~ om het gebouw binnen te gaan.',
+    ['press_to_exit'] = 'Druk op ~INPUT_CONTEXT~ om het gebouw te verlaten.',
+}
