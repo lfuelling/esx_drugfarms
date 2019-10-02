d0ba87365a95a957c8f005d25ac6f9b62eb5cc97
Lerk <lukas@k40s.net>
2019-10-03 00:41:36 +0200

    add hint for dutch translation to config

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.Locale = 'en' -- available: en, es
+Config.Locale = 'en' -- available: en, es, nl
 Config.JobId = 'police' -- people with this job can't get in
 Config.DrawBlips = true -- if true, map markers are visible
 
