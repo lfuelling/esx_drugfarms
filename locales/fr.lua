3894c34156b04714ff916bdf101e02069872dd24
Alexandre "Moutarde" S <moutardea@gmail.com>
2020-06-11 12:37:14 +0200

    French translation

---
+++ locales/fr.lua
@@ -0,0 +1,13 @@
+Locales['fr'] = {
+    ['no_access'] = "~r~Vous ne pouvez pas entrer.",
+    ['weed_farm'] = "Ferme de Weed",
+    ['counterfeit'] = "Contrefait",
+    ['document_forge'] = "Falsification de documents",
+    ['meth_lab'] = "Labo de Meth",
+    ['coke_lab'] = "Labo de Coke",
+    ['bunker'] = "Bunker",
+    ['doomsday_facility'] = "Centre de l'apocalypse",
+    ['biker_clubhouse'] = "Clubhouse",
+    ['press_to_enter'] = 'Appuyez sur ~INPUT_CONTEXT~ pour entrer dans le batiment.',
+    ['press_to_exit'] = 'Appuyez sur ~INPUT_CONTEXT~ pour sortir du batiment.',
+}
